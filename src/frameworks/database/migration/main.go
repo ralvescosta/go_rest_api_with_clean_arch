8f2533a3507b301e081fd678b6429763b960db9e
Rafael Alves <rafael.rac.mg@gmail.com>
2020-12-13 09:01:50 -0300

    feat: migration

--- src/frameworks/database/migration/main.go
+++ src/frameworks/database/migration/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"restapi/src/frameworks/database"
+)
 
 func main() {
-	fmt.Println("Migrate")
+	dbConn, err := database.DbConnection()
+	if err != nil {
+		panic(err)
+	}
+	defer dbConn.Close()
+
+	fmt.Println("Running migrations....")
+
+	_, err = dbConn.Exec(`
+		CREATE TABLE books
+		(
+			id SERIAL NOT NULL PRIMARY KEY,
+			title varchar(255) NOT NULL,
+			author varchar(255) NOT NULL,
+			publishing_company varchar(255) NOT NULL,
+			edition int,
+			created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			deleted_at TIMESTAMP WITH TIME ZONE
+		);
+	`)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Migration run successfully")
+
 }

cd6aaee4b035d29ea070003fe558bd7cfe902844
Rafael Alves <rafael.rac.mg@gmail.com>
2020-12-15 21:03:34 -0300

    refactor: create book usecase and test

--- src/interfaces/create_book_controller_test.go
+++ src/interfaces/create_book_controller_test.go
@@ -9,16 +9,18 @@ import (
 	"testing"
 )
 
-type createBookUsecaseMock struct{}
-
-func (*createBookUsecaseMock) Create(bookDTO *entities.BookDTO) *shared.HTTPResponse {
-	return shared.HTTPCreated()
-}
-func NewCreateBookUsecaseMock() applications.ICreateBookUsecase {
-	return &createBookUsecaseMock{}
+type createBookUsecaseMock struct {
+	createdReturn *shared.HTTPResponse
 }
 
-func TestCreateBookControllerSuccessfully(t *testing.T) {
+func (mock *createBookUsecaseMock) Create(bookDTO *entities.BookDTO) *shared.HTTPResponse {
+	return mock.createdReturn
+}
+func NewCreateBookUsecaseMock(createdReturn *shared.HTTPResponse) applications.ICreateBookUsecase {
+	return &createBookUsecaseMock{createdReturn: createdReturn}
+}
+
+func TestCreateBookControllerSuccessfullyWithoutBody(t *testing.T) {
 
 	req := httptest.NewRequest("POST", "http://example.com/foo",
 		strings.NewReader(`{
@@ -29,12 +31,62 @@ func TestCreateBookControllerSuccessfully(t *testing.T) {
 	}`))
 	res := httptest.NewRecorder()
 
-	controller := CreateBookController{Usecase: NewCreateBookUsecaseMock()}
+	controller := CreateBookController{Usecase: NewCreateBookUsecaseMock(shared.HTTPCreated())}
 	controller.Handler(res, req)
 
 	result := res.Result()
 
 	if result.StatusCode != 201 {
 		t.Errorf("Should return status code 201 but received %d", result.StatusCode)
+	}
+}
+
+func TestCreateBookControllerSuccessfullyWithBody(t *testing.T) {
+	req := httptest.NewRequest("POST", "http://example.com/foo",
+		strings.NewReader(`{
+			"title": "title",
+			"author": "author",
+			"publishingCompany": "publishingCompany",
+			"edition": 1
+	}`))
+	res := httptest.NewRecorder()
+
+	controller := CreateBookController{Usecase: NewCreateBookUsecaseMock(shared.HTTPSuccess(""))}
+	controller.Handler(res, req)
+
+	result := res.Result()
+
+	if result.StatusCode != 200 {
+		t.Errorf("Should return status code 200 but received %d", result.StatusCode)
+	}
+}
+
+func TestCreateBookControllerErrNoBody(t *testing.T) {
+	req := httptest.NewRequest("POST", "http://example.com/foo", nil)
+	res := httptest.NewRecorder()
+
+	controller := CreateBookController{Usecase: NewCreateBookUsecaseMock(shared.HTTPCreated())}
+	controller.Handler(res, req)
+
+	result := res.Result()
+
+	if result.StatusCode != 400 {
+		t.Errorf("Should return status code 400 but received %d", result.StatusCode)
+	}
+}
+func TestCreateBookControllerErrBodyWrongFormat(t *testing.T) {
+	req := httptest.NewRequest("POST", "http://example.com/foo",
+		strings.NewReader(`{
+			"title": "title"
+	}`))
+	res := httptest.NewRecorder()
+
+	controller := CreateBookController{Usecase: NewCreateBookUsecaseMock(shared.HTTPCreated())}
+	controller.Handler(res, req)
+
+	result := res.Result()
+
+	if result.StatusCode != 400 {
+		t.Errorf("Should return status code 400 but received %d", result.StatusCode)
 	}
 }
